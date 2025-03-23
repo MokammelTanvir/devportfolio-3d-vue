d8a761a61b5d0d9ddabfbf6b27615a1db1c4b0f6
Mokammel Hossain Tanvir <mokammeltanvir01@gmail.com>
2025-03-23 06:19:13 +0600

    Add core Vue application files

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<script setup>
+import HeroSection from "./components/HeroSection.vue";
+import AboutSection from "./components/AboutSection.vue";
+import SkillsSection from "./components/SkillsSection.vue";
+import ProjectsSection from "./components/ProjectsSection.vue";
+import ContactSection from "./components/ContactSection.vue";
+import NavBar from "./components/NavBar.vue";
+import FooterSection from "./components/FooterSection.vue";
+</script>
+
+<template>
+  <div class="min-h-screen bg-gray-900 text-white">
+    <NavBar />
+
+    <main>
+      <HeroSection id="home" />
+      <AboutSection id="about" />
+      <SkillsSection id="skills" />
+      <ProjectsSection id="projects" />
+      <ContactSection id="contact" />
+    </main>
+
+    <FooterSection />
+  </div>
+</template>
+
+<style>
+/* Global styles can be added here */
+</style>
